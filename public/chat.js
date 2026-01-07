7949e62ef335ee2f046b10d2c46407faac880b2c
Abhi <sutharabhimanyu70@gmail.com>
2026-01-07 16:19:43 +0530

    remove dp from message bubbles whatsapp style

--- public/chat.js
+++ public/chat.js
@@ -2,44 +2,38 @@ const socket = io();
 
 const name = localStorage.getItem("username");
 const dp = localStorage.getItem("dp");
-
-// 👇 किस user से chat है (URL से)
 const params = new URLSearchParams(window.location.search);
 const to = params.get("user");
 
-if (!name || !to) {
-  location.href = "/chats.html";
-}
+if (!name || !to) location.href = "/chats.html";
 
 // HEADER
 document.getElementById("chatName").innerText = to;
 document.getElementById("headerDp").src =
   dp || "https://i.imgur.com/6VBx3io.png";
 
-// JOIN (register user on server)
 socket.emit("join", { name, dp });
 
 const messages = document.getElementById("messages");
 const msgInput = document.getElementById("msg");
 
-/* ========= SEND MESSAGE ========= */
+/* ===== SEND ===== */
 function sendMsg() {
   const msg = msgInput.value.trim();
   if (!msg) return;
 
   addMsg("You", msg, "me");
 
-  // 🔥 IMPORTANT FIX: `to` भेज रहे हैं
   socket.emit("private-msg", {
-    to: to,
+    to,
     from: name,
-    msg: msg
+    msg
   });
 
   msgInput.value = "";
 }
 
-// ENTER PRESS
+// ENTER SEND
 msgInput.addEventListener("keydown", e => {
   if (e.key === "Enter") {
     e.preventDefault();
@@ -47,16 +41,23 @@ msgInput.addEventListener("keydown", e => {
   }
 });
 
-/* ========= RECEIVE MESSAGE ========= */
+/* ===== RECEIVE ===== */
 socket.on("private-msg", d => {
   addMsg(d.from, d.msg, "other");
 });
 
-/* ========= UI ========= */
+/* ===== UI ===== */
 function addMsg(user, msg, type) {
   const div = document.createElement("div");
   div.className = "msg " + type;
-  div.innerHTML = `<b>${user}</b><br>${msg}`;
+
+  // 🔥 NO DP HERE
+  div.innerHTML = `
+    <div class="bubble">
+      <b>${user}</b><br>${msg}
+    </div>
+  `;
+
   messages.appendChild(div);
   messages.scrollTop = messages.scrollHeight;
 }
